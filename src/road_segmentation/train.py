720915fe0436f5add994f771d8b0508f865c01c5
leolibe <leoli@stud.ntnu.no>
2026-05-01 12:46:14 +0200

    Add new mask images for examples
    - Added PNG mask images for various configurations:
      - 33-2-464-214-13_2304_2304.png
      - 33-2-464-216-01_1792_4096.png
      - 33-2-464-216-22_2304_1024.png
      - 33-2-464-216-31_512_2816.png
      - 33-2-464-217-00_3328_1536.png
    
    These images are part of the example dataset and are intended for use in testing and demonstration purposes.

--- src/road_segmentation/train.py
+++ src/road_segmentation/train.py
@@ -3,6 +3,23 @@ from tensorflow.keras.optimizers.legacy import Adam
 from losses import bce_dice_loss, dice_coef, iou_score
 from dataset import make_dataset
 from unet import unet
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.keras.layers import Conv2DTranspose
+
+
+class Conv2DTransposeFixed(Conv2DTranspose):
+    def __init__(self, *args, **kwargs):
+        kwargs.pop("groups", None)
+        super().__init__(*args, **kwargs)
+
+
+def load_model_fixed(model_path):
+    return load_model(
+        model_path,
+        custom_objects={"Conv2DTranspose": Conv2DTransposeFixed},
+        compile=False
+    )
 
 def train_model(train_ds, val_ds, input_shape):
     model = unet(input_shape, output_layer=1)
